eee9f91ee78fbead7e10d3f9d7feb480d70ff1be
zou-mono <zou.mono@gmail.com>
2017-10-12 17:58:03 +0800

    add TeX-fold-mode.

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -9,6 +9,7 @@
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'TeX-fold-mode)
 
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 (setq reftex-plug-into-AUCTeX t)
