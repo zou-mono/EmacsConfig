2545f8e536e2f30ab2213f4f68d001c572cb1802
zou-mono <zou.mono@gmail.com>
2017-10-14 14:18:05 +0800

    add init-utils.el to store global functions and macros.

--- init.el
+++ init.el
@@ -1,39 +1,7 @@
 ;; init.el
-;;(package-initialize)
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;;(defvar emacs-conf-path "~/.emacs.d/")
-;;(load (concat emacs-conf-path "init.el"))
-
-;; Function to collect information of packages.
-(defvar missing-packages-list nil
-  "List of packages that `try-require' can't find.")
-
-(defvar package-init-statistic nil "Package loading statistics")
-
-;; attempt to load a feature/library, failing silently
-(defun try-require (feature &optional click)
-  "Attempt to load a library or module. Return true if the
-library given as argument is successfully loaded. If not, instead
-of an error, just add the package to a list of missing packages."
-  (condition-case err
-      ;; protected form
-      (let ((timestamp (current-time))
-            (package (if (stringp feature) feature (symbol-name feature))))
-        (if (stringp feature)
-            (load-library feature)
-          (require feature))
-        (if click
-            (add-to-list 'package-init-statistic
-                         (cons (if (stringp feature) feature (symbol-name feature))
-                               (float-time (time-since timestamp)))))
-        (message "Checking for library `%s'... Found, cost %.2f seconds"
-                 feature (float-time (time-since timestamp))))
-    ;; error handler
-    (file-error  ; condition
-     (progn
-       (message "Checking for library `%s'... Missing" feature)
-       (add-to-list 'missing-packages-list feature 'append))
-     nil)))
+(require 'init-utils)
 
 (custom-set-variables
  '(ansi-color-faces-vector
@@ -163,6 +131,9 @@ of an error, just add the package to a list of missing packages."
 
 (load (expand-file-name "lisp/loaddefs.el" user-emacs-directory) nil t t)
 
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
 (let ((ts-init (current-time)))
   (setq missing-packages-list nil
         package-init-statistic nil)
@@ -190,9 +161,6 @@ of an error, just add the package to a list of missing packages."
                ". Refer to `missing-packages-list` for missing packages."
              ".")))
 
-;;----------------------------------------------------------------------------
-;; Load configs for specific features and modes
-;;----------------------------------------------------------------------------
 ;; (require 'init-base) ;; base settings
 ;; (require 'init-tabbar)
 ;; (require 'init-layout) ;; layout settings
