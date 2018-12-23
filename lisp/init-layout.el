22742135de441f0015f97b8cc452ca7fdccf84e4
zou-mono <zou.mono@gmail.com>
2018-12-23 22:55:13 +0800

    add web layout.

--- lisp/init-layout.el
+++ lisp/init-layout.el
@@ -15,12 +15,13 @@
   (add-to-list 'purpose-user-mode-purposes '(inferior-ess-mode . r-repl))
   (purpose-compile-user-configuration)
 
-  (purpose-load-window-layout-file (expand-file-name "layouts/Rlayout.window-layout" user-emacs-directory))
-  (purpose-load-frame-layout-file (expand-file-name "layouts/Rlayout.frame-layout" user-emacs-directory)))
+  (purpose-load-window-layout-file (expand-file-name "layouts/R-layout.window-layout" user-emacs-directory))
+  (purpose-load-frame-layout-file (expand-file-name "layouts/R-layout.frame-layout" user-emacs-directory)))
 
 ;;;###autoload
 (defun perspective/lisp()
   (interactive)
+  
   (purpose-mode)
   ;; lisp
   (add-to-list 'purpose-user-mode-purposes '(lisp-interaction-mode . lisp-interaction))
@@ -54,10 +55,26 @@
   (purpose-load-window-layout-file (expand-file-name "layouts/tex-layout.window-layout" user-emacs-directory))
   (purpose-load-frame-layout-file (expand-file-name "layouts/tex-layout.frame-layout" user-emacs-directory)))
 
+;;;###autoload
+(defun perspective/web()
+  (interactive)
+  (purpose-mode)
+  ;; R
+  (add-to-list 'purpose-user-mode-purposes '(js2-mode . web))
+  (add-to-list 'purpose-user-mode-purposes '(web-mode . web))
+  (add-to-list 'purpose-user-mode-purposes '(sgml-mode . web))
+  (add-to-list 'purpose-user-mode-purposes '(css-mode . web))
+  (add-to-list 'purpose-user-mode-purposes '(indium-repl-mode . js-repl))
+  (purpose-compile-user-configuration)
+
+  (purpose-load-window-layout-file (expand-file-name "layouts/web-layout.window-layout" user-emacs-directory))
+  (purpose-load-frame-layout-file (expand-file-name "layouts/web-layout.frame-layout" user-emacs-directory)))
+
 (define-key perspective-map (kbd "R") 'perspective/R)
 (define-key perspective-map (kbd "l") 'perspective/lisp)
 (define-key perspective-map (kbd "p") 'perspective/python)
 (define-key perspective-map (kbd "t") 'perspective/tex)
+(define-key perspective-map (kbd "w") 'perspective/web)
 
 (provide 'init-layout)
 
