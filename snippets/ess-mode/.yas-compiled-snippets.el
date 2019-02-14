ca6731b9a8f509e0d74d37e744c2ec553ddeedb2
zou-mono <zou.mono@gmail.com>
2019-02-14 18:17:43 +0800

    add some snippets for latex-mode.

--- snippets/ess-mode/.yas-compiled-snippets.el
+++ snippets/ess-mode/.yas-compiled-snippets.el
@@ -2,17 +2,17 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'ess-mode
-                     '(("tt" "\n`(current-time-string)`\n`(make-string (- fill-column 3) ?\\-)`\n$0" "test" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/test" nil nil)
-                       ("spd" "\n# Connect to ace/spade\nspd_con <- dbConnect(\n  dbDriver(\"PostgreSQL\"),\n  host     = Sys.getenv(\"SPADE_URL\"),\n  port     = Sys.getenv(\"SPADE_PORT\"),\n  dbname   = Sys.getenv(\"SPADE_DBNAME\"),\n  user     = Sys.getenv(\"SPADE_USERNAME\"),\n  password = Sys.getenv(\"SPADE_PASSWORD\")\n)\n\n$0\n\n# Close connection to ace/spade\ndbDisconnect(spd_con)\n" "connect-to-spade" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/spade-connection" nil nil)
-                       ("libs" "\nlibrary(arm)\nlibrary(RPostgreSQL)\nlibrary(brocks)\nlibrary(tidyverse)" "Load common libraries" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/libraries" nil nil)
+                     '(("tt" "`(current-time-string)`\n`(make-string (- fill-column 3) ?\\-)`\n$0" "test" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/test" nil nil)
+                       ("spd" "# Connect to ace/spade\nspd_con <- dbConnect(\n  dbDriver(\"PostgreSQL\"),\n  host     = Sys.getenv(\"SPADE_URL\"),\n  port     = Sys.getenv(\"SPADE_PORT\"),\n  dbname   = Sys.getenv(\"SPADE_DBNAME\"),\n  user     = Sys.getenv(\"SPADE_USERNAME\"),\n  password = Sys.getenv(\"SPADE_PASSWORD\")\n)\n\n$0\n\n# Close connection to ace/spade\ndbDisconnect(spd_con)\n" "connect-to-spade" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/spade-connection" nil nil)
+                       ("libs" "library(arm)\nlibrary(RPostgreSQL)\nlibrary(brocks)\nlibrary(tidyverse)" "Load common libraries" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/libraries" nil nil)
                        ("if" "if (${1:condition}) {\n  `yas/selected-text`$0\n}" "if (...) { ... }" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/if" nil nil)
-                       ("ggscat" "\nggplot($1, aes($2, $3)) +\n  geom_point() +\n  geom_smooth() +\n  theme_bw()\n" "ggplot scatter" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/ggplot_scatter" nil nil)
+                       ("ggscat" "ggplot($1, aes($2, $3)) +\n  geom_point() +\n  geom_smooth() +\n  theme_bw()\n" "ggplot scatter" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/ggplot_scatter" nil nil)
                        ("f" "${1:name} <- function(${2:args}) {\n  `yas/selected-text`$0\n}" "FUNC <- function(...) { ... }" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/function" nil nil)
                        ("for" "for (${1:i} in ${2:vector}) {\n  `yas/selected-text`$0\n}" "for ( ... in ...) { ... }" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/for" nil nil)
-                       ("s" "\n### ${1:} ${1:$(\n  \n  make-string (\n    ceiling (/ (- fill-column 3 (string-width yas-text)) 1.0)\n  ) ?\\-\n\n  )}\n\n$0\n" "comment divider" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/comment-divider" nil nil)
-                       ("cb" "\n### `(make-string (- fill-column 3) ?\\-)`\n### $0\n### `(make-string (- fill-column 3) ?\\-)`" "comment block" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/comment-block" nil nil)
+                       ("s" "### ${1:} ${1:$(\n  \n  make-string (\n    ceiling (/ (- fill-column 3 (string-width yas-text)) 1.0)\n  ) ?\\-\n\n  )}\n\n$0\n" "comment divider" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/comment-divider" nil nil)
+                       ("cb" "### `(make-string (- fill-column 3) ?\\-)`\n### $0\n### `(make-string (- fill-column 3) ?\\-)`" "comment block" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/comment-block" nil nil)
                        ("T" "TRUE" "TRUE" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/TRUE" nil nil)
                        ("F" "FALSE" "FALSE" nil nil nil "/home/mono/.emacs.d/snippets/ess-mode/FALSE" nil nil)))
 
 
-;;; Do not edit! File generated at Fri Sep  7 17:39:45 2018
+;;; Do not edit! File generated at Thu Feb 14 16:31:58 2019
--- snippets/latex-mode/.yas-compiled-snippets.el
+++ snippets/latex-mode/.yas-compiled-snippets.el
@@ -2,14 +2,19 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'latex-mode
-                     '(("tikz" "\n\\begin{tikzpicture}\n$0\n\\end{tikzpicture}" "tikzpicture environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/tikzpicture-env" nil nil)
-                       ("tblx" "\n\\begin{table}[!ht]\\centering\n\\caption{\\label{tbl:}} \n\\begin{tabularx}{\\textwidth}{|X|X|}\\\\\\\\\\hline\n\\multicolumn{1}{|c|}{\\bfseries $0} & \\multicolumn{1}{c|}{\\bfseries $1}\\\\\\\\\\hline\n  & \\\\\\\\\n\\hline\n\\end{tabularx}\n\\end{table}\n" "tabularx environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/tabularx-env" nil nil)
-                       ("scope" "\n\\begin{scope}\n$0\n\\end{scope}" "scope environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/scope-env" nil nil)
+                     '(("tikz" "\\begin{tikzpicture}\n$0\n\\end{tikzpicture}" "tikzpicture environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/tikzpicture-env" nil nil)
+                       ("tblx" "\\begin{table}[!ht]\\centering\n\\caption{\\label{tbl:}} \n\\begin{tabularx}{\\textwidth}{|X|X|}\\\\\\\\\\hline\n\\multicolumn{1}{|c|}{\\bfseries $0} & \\multicolumn{1}{c|}{\\bfseries $1}\\\\\\\\\\hline\n  & \\\\\\\\\n\\hline\n\\end{tabularx}\n\\end{table}\n" "tabularx environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/tabularx-env" nil nil)
+                       ("subfig" "\\begin{figure}\\centering\n    \\subfloat[$1]\n    {\\includegraphics[width=0.48\\textwidth]{$2}}\\vspace{1pt} \n    \\subfloat[$3]\n    {\\includegraphics[width=0.48\\textwidth]{$4}}\n    \\caption{$5}\n\\end{figure}\n" "subfig environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/subfig-env" nil nil)
+                       ("scope" "\\begin{scope}\n$0\n\\end{scope}" "scope environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/scope-env" nil nil)
                        ("rnw" "<<$0>>=\n@\n" "rnw-block" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/rmarkdown-chunk" nil nil)
-                       ("ltblx" "\n\\begin{longtabu} to \\textwidth {|c|X[1,c]|}\n\\caption{\\label{tbl:}}\n\\hline\n\\multicolumn{1}{|c|}{\\bfseries $0} & \\multicolumn{1}{c|}{\\bfseries $1}\\\\\\\\\\hline\n  & \\\\\\\\\n\\hline\n\\end{longtabu}\n\n" "longtabu environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/longtablex-env" nil nil)
-                       ("ctbl" "\n\\begin{table}[!ht]\\centering \n\\caption{\\label{tbl:}}\n\\begin{tabular}{|c|l|}\n\\hline\n $0 & \\\\\\\\\n\\hline\n\\end{tabular}\n\\end{table}\n" "common table environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/commontable-env" nil nil)
-                       ("center" "\n\\begin{center}\n$0\n\\end{center}" "centering environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/center-env" nil nil)
-                       ("btbl" "\n\\begin{table} \\centering \n\\caption{{\\label{tbl:}}}\n\\begin{tabular}{|c|c|}\n  \\toprule\n  \\rowcolor{LightCyan}\n  \\multicolumn{1}{|c|}{\\textbf{参数名称}} & \\multicolumn{1}{c|}{\\textbf{作用}} \\\\\\\\ \\hline\n$0 & \\\\\\\\ \\hline\n& \\\\\\\\\n  \\bottomrule\n \\end{tabular}\n\\end{table}\n" "book table environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/booktable-env" nil nil)))
+                       ("ov" "\\begin{overlayarea}{\\textwidth}{\\textheight}\n\\only<${1:1}>{\n$2\n}\n\\end{overlayarea}\n" "overlayarea environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/overlayarea-env" nil nil)
+                       ("ltblx" "\\begin{longtabu} to \\textwidth {|c|X[1,c]|}\n\\caption{\\label{tbl:}}\n\\hline\n\\multicolumn{1}{|c|}{\\bfseries $0} & \\multicolumn{1}{c|}{\\bfseries $1}\\\\\\\\\\hline\n  & \\\\\\\\\n\\hline\n\\end{longtabu}\n\n" "longtabu environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/longtablex-env" nil nil)
+                       ("it" "\\begin{itemize}\n\\item<1-> $0\n\\end{itemize}\n" "itemize environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/item-env" nil nil)
+                       ("fr" "\\begin{frame}[t]{$0}\n$1\n\\end{frame}\n" "frame environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/frame-env" nil nil)
+                       ("fig" "\\begin{figure}\n    \\centering\n    \\includegraphics[width=\\textwidth]{$1}\n    \\caption{$2}\n\\end{figure}\n" "figure environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/figure-env" nil nil)
+                       ("ctbl" "\\begin{table}[!ht]\\centering \n\\caption{\\label{tbl:}}\n\\begin{tabular}{|c|l|}\n\\hline\n $0 & \\\\\\\\\n\\hline\n\\end{tabular}\n\\end{table}\n" "common table environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/commontable-env" nil nil)
+                       ("center" "\\begin{center}\n$0\n\\end{center}" "centering environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/center-env" nil nil)
+                       ("btbl" "\\begin{table} \\centering \n\\caption{{\\label{tbl:}}}\n\\begin{tabular}{|c|c|}\n  \\toprule\n  \\rowcolor{LightCyan}\n  \\multicolumn{1}{|c|}{\\textbf{参数名称}} & \\multicolumn{1}{c|}{\\textbf{作用}} \\\\\\\\ \\hline\n$0 & \\\\\\\\ \\hline\n& \\\\\\\\\n  \\bottomrule\n \\end{tabular}\n\\end{table}\n" "book table environment" nil nil nil "/home/mono/.emacs.d/snippets/latex-mode/booktable-env" nil nil)))
 
 
-;;; Do not edit! File generated at Fri Sep  7 17:39:45 2018
+;;; Do not edit! File generated at Thu Feb 14 16:31:58 2019
